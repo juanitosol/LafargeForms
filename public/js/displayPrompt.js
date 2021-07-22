7bd013e3cae5b086410ea91b45fa97045708d863
juanitosol <jfdeleon@uwaterloo.ca>
2021-07-22 16:27:02 -0400

    more changes

--- public/js/displayPrompt.js
+++ public/js/displayPrompt.js
@@ -1,12 +1,14 @@
-function displayPrompt(promptId, validateId) {
+function displayPrompt(promptId, validateId, priorityId) {
     var x = document.getElementById(promptId);
     if (x.style.display === "flex") {
         x.style.display = "none";
         document.getElementById(validateId).required = false;
+        document.getElementById(priorityId).required = false;
     }
     else {
         x.style.display = "flex";
         x.style.justifyContent = "flex-start"
         document.getElementById(validateId).required = true;
+        document.getElementbyId(priorityId).required = true;
     }
 }

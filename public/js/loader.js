da22d71955476952d7ac0876579ce94c49fe3c8a
juanitosol <jfdeleon@uwaterloo.ca>
2021-07-28 17:30:55 -0400

    almost Done!

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,1 +1,16 @@
-//loader js
+$(document).ready(function () {
+    const lockModal = $("#lock-modal");
+    const loadingCircle = $("#loader");
+    const form = $("#primaryForm");
+
+    form.on('submit', function (e) {
+        // e.preventDefault();
+
+        lockModal.css("display", "block");
+        loadingCircle.css("display", "block");
+
+        // form.children("input").each(function () {
+        //     $(this).attr("readonly", true);
+        // })
+    })
+})

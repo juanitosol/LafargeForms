66745a8d58f63baaed62eed7df8da21dd52b2265
juanitosol <jfdeleon@uwaterloo.ca>
2021-08-27 16:07:13 -0400

    Minor Fixes

--- controllers/secondary4Checklist.js
+++ controllers/secondary4Checklist.js
@@ -52,16 +52,16 @@ module.exports.postSecondary4Checklist = async (req, res) => {
         d4278: d4278, d4031: d4031, d4032: d4032,
         d4117: d4117, d4123: d4123, d4116: d4116,
         d4046: d4046, d4047: d4047, d4118: d4118,
-        d4077: d4077, d2007: d2007, d2010: d2010,
-        d2025: d2025, d2023: d2023, d2270: d2270,
+        d4077: d4077, d2007: d2007, d2025: d2025,
+        d2023: d2023, d2270: d2270,
     }
     const guards = {
         g4076: g4076, g4079: g4079, g4800: g4800,
         g4278: g4278, g4031: g4031, g4032: g4032,
         g4117: g4117, g4123: g4123, g4116: g4116,
         g4046: g4046, g4047: g4047, g4118: g4118,
-        g4077: g4077, g2007: g2007, g2010: g2010,
-        g2025: g2025, g2023: g2023, g2270: g2270,
+        g4077: g4077, g2007: g2007, g2025: g2025,
+        g2023: g2023, g2270: g2270,
     }
     //analyzes if defectives are checked or if guards are unchecked
     const defectArray = []; //array is empty, but will systematically input any defects or issues into it
@@ -91,11 +91,12 @@ module.exports.postSecondary4Checklist = async (req, res) => {
         valueInputOption: "USER_ENTERED", //This will convert data into proper formats (like date into date not string), so won't take raw data
         resource: {
             values: [
-                [date, employee, shift,
+                [
+                    date, employee, shift,
                     gt4076, gt4079, gt4800, gt4278, gt4031,
                     gt4032, gt4117, gt4123, gt4116, gt4046,
-                    gt4047, gt4118, gt4077, gt2007, gt2010,
-                    gt2025, gt2023, gt2270,
+                    gt4047, gt4118, gt4077, gt2007, gt2025,
+                    gt2023, gt2270,
                     isAllGuardsChecked, defectsExist, '-',
                 ], //these are the values that will be input into a single row, order matters
             ]
@@ -105,20 +106,20 @@ module.exports.postSecondary4Checklist = async (req, res) => {
     const allDefects = [
         hd4076, hd4079, hd4800, hd4278, hd4031,
         hd4032, hd4117, hd4123, hd4116, hd4046,
-        hd4047, hd4118, hd4077, hd2007, hd2010,
-        hd2025, hd2023, hd2270,
+        hd4047, hd4118, hd4077, hd2007, hd2025,
+        hd2023, hd2270,
         hg4076, hg4079, hg4800, hg4278, hg4031,
         hg4032, hg4117, hg4123, hg4116, hg4046,
-        hg4047, hg4118, hg4077, hg2007, hg2010,
-        hg2025, hg2023, hg2270,
+        hg4047, hg4118, hg4077, hg2007, hg2025,
+        hg2023, hg2270,
     ]
 
     const prefixes = [
         '1744076', '1744079', '1744800', '1444278',
         '1444031', '1444032', '1444117', '1444123',
         '1444116', '1444046', '1444047', '1444118',
-        '1744077', '1442007', '1442010', '1442025',
-        '1742023', '1442270'
+        '1744077', '1442007', '1442025', '1742023',
+        '1442270'
     ]
 
 
@@ -133,8 +134,8 @@ module.exports.postSecondary4Checklist = async (req, res) => {
     const allPriorities = [
         p4076, p4079, p4800, p4278, p4031,
         p4032, p4117, p4123, p4116, p4046,
-        p4047, p4118, p4077, p2007, p2010,
-        p2025, p2023, p2270,
+        p4047, p4118, p4077, p2007, p2025,
+        p2023, p2270,
     ]
     const allPrioritiesString = allPriorities.filter(Boolean).join("\n");
 
